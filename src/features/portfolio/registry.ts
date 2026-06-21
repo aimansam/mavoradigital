a4991f09141b7d3e568a22af1d73e965fb60ac23
Mavora Labs <dev@mavoralabs.com>
2026-06-21 22:48:04 +0000

    feat: add 5 HTML portfolio demos (car-wash, ejen-automobile, ejen-hartanah, kopi-jiran, salon)

--- src/features/portfolio/registry.ts
+++ src/features/portfolio/registry.ts
@@ -1,12 +1,28 @@
 import BurgerDemo from "./demos/burger-shop/BurgerDemo.astro";
-import CoffeeDemo from "./demos/coffee-shop/CoffeeDemo.astro";
+import CarWashDemo from "./demos/car-wash/CarWashDemo.astro";
+import EjenAutomobileDemo from "./demos/ejen-automobile/EjenAutomobileDemo.astro";
+import EjenHartanahDemo from "./demos/ejen-hartanah/EjenHartanahDemo.astro";
+import KopiJiranDemo from "./demos/kopi-jiran/KopiJiranDemo.astro";
+import SalonDemo from "./demos/salon/SalonDemo.astro";
 
 export const portfolioDemoRegistry = {
   "burger-shop": {
     component: BurgerDemo,
   },
-  "coffee-shop": {
-    component: CoffeeDemo,
+  "kopi-jiran": {
+    component: KopiJiranDemo,
+  },
+  "car-wash": {
+    component: CarWashDemo,
+  },
+  "ejen-automobile": {
+    component: EjenAutomobileDemo,
+  },
+  "ejen-hartanah": {
+    component: EjenHartanahDemo,
+  },
+  "salon": {
+    component: SalonDemo,
   },
 } as const;
 
