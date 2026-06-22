3758529332279a961aa07c9b77aa2288c56cd043
Mavora Labs <dev@mavoralabs.com>
2026-06-22 02:38:33 +0000

    feat: add v2 demos for ejen-automobile and ejen-hartanah (2nd HTML version each)

--- src/features/portfolio/registry.ts
+++ src/features/portfolio/registry.ts
@@ -2,6 +2,8 @@ import BurgerDemo from "./demos/burger-shop/BurgerDemo.astro";
 import CarWashDemo from "./demos/car-wash/CarWashDemo.astro";
 import EjenAutomobileDemo from "./demos/ejen-automobile/EjenAutomobileDemo.astro";
 import EjenHartanahDemo from "./demos/ejen-hartanah/EjenHartanahDemo.astro";
+import EjenAutomobile2Demo from "./demos/ejen-automobile-2/EjenAutomobile2Demo.astro";
+import EjenHartanah2Demo from "./demos/ejen-hartanah-2/EjenHartanah2Demo.astro";
 import KopiJiranDemo from "./demos/kopi-jiran/KopiJiranDemo.astro";
 import SalonDemo from "./demos/salon/SalonDemo.astro";
 
@@ -20,6 +22,12 @@ export const portfolioDemoRegistry = {
   },
   "ejen-hartanah": {
     component: EjenHartanahDemo,
+  },
+  "ejen-automobile-2": {
+    component: EjenAutomobile2Demo,
+  },
+  "ejen-hartanah-2": {
+    component: EjenHartanah2Demo,
   },
   "salon": {
     component: SalonDemo,
