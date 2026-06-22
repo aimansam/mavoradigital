2ae970cbe805afb08fde2c9932ab9dcf92f7e4da
Mavora Labs <dev@mavoralabs.com>
2026-06-22 09:35:17 +0000

    refactor: remove agent demos from portfolio
    - Remove ejen-automobile, ejen-automobile-2, ejen-hartanah, ejen-hartanah-2
    - Keep demo files for potential future use
    - Portfolio now: Landing Page (3), Business Website (1), Custom Build (1)

--- src/features/portfolio/registry.ts
+++ src/features/portfolio/registry.ts
@@ -1,9 +1,5 @@
 import BurgerDemo from "./demos/burger-shop/BurgerDemo.astro";
 import CarWashDemo from "./demos/car-wash/CarWashDemo.astro";
-import EjenAutomobileDemo from "./demos/ejen-automobile/EjenAutomobileDemo.astro";
-import EjenHartanahDemo from "./demos/ejen-hartanah/EjenHartanahDemo.astro";
-import EjenAutomobile2Demo from "./demos/ejen-automobile-2/EjenAutomobile2Demo.astro";
-import EjenHartanah2Demo from "./demos/ejen-hartanah-2/EjenHartanah2Demo.astro";
 import KopiJiranDemo from "./demos/kopi-jiran/KopiJiranDemo.astro";
 import SalonDemo from "./demos/salon/SalonDemo.astro";
 
@@ -16,18 +12,6 @@ export const portfolioDemoRegistry = {
   },
   "car-wash": {
     component: CarWashDemo,
-  },
-  "ejen-automobile": {
-    component: EjenAutomobileDemo,
-  },
-  "ejen-hartanah": {
-    component: EjenHartanahDemo,
-  },
-  "ejen-automobile-2": {
-    component: EjenAutomobile2Demo,
-  },
-  "ejen-hartanah-2": {
-    component: EjenHartanah2Demo,
   },
   "salon": {
     component: SalonDemo,
